f028bba863f247a70437f24cc12037d209b945e7
John Paolo Yao <38048884+paoloyao@users.noreply.github.com>
2023-01-07 10:39:31 +0800

    Update fetchComments.ts

--- twitter-clone/utils/fetchComments.ts
+++ twitter-clone/utils/fetchComments.ts
@@ -1,5 +1,5 @@
 export const fetchComments = async (tweetId: string) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getComments?tweetId=${tweetId}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getComments?tweetId=${tweetId}`);
   const comments: Comment[] = await res.json();
   return comments;
 };
